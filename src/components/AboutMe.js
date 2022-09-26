e8afa03e9fafffce4d1551cc2b5754c8f724d1f4
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-09-26 09:19:03 +0100

    About me page created

---
+++ src/components/AboutMe.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+const AboutMe = () => {
+
+    return (
+        <main>
+            <section class="about-me line">
+                <p>
+                    About me
+                </p>
+            </section>
+            <section class="projects line">
+            <p>
+                Projects
+            </p>
+            </section>
+            <section class="get-in-touch line">
+                <p>
+                    Get in touch
+                </p>
+            </section>
+        </main>
+    );
+}
+
+export default AboutMe;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,32 +1,55 @@
 import React, {useState} from "react";
+import AboutMe from "./AboutMe";
 
 const Portfolio = () => {
 
-    return (
+    //Declaring useState variables
+    const [aboutMe, updateAboutMe] = useState(false);
+    const [projects, updateProjects] = useState(false);
+    const [getInTouch, updateGetInTouch] = useState(false);
 
-    <main>
-        <section class="about-me line">
-            <p>
-                <span class="about-me__letter1">A</span><span class="about-me__letter2">b</span><span class="about-me__letter3">o</span><span class="about-me__letter4">u</span><span class="about-me__letter5">t </span>
-                &nbsp;
-                <span class="about-me__letter6">m</span><span class="about-me__letter7">e</span>
-            </p>
-        </section>
-        <section class="projects line">
-        <p>
-                <span class="projects__letter4">P</span><span class="projects__letter3">r</span><span class="projects__letter2">o</span><span class="projects__letter1">j</span><span class="projects__letter1">e </span><span class="projects__letter2">c</span><span class="projects__letter3">t</span><span class="projects__letter4">s</span>
-            </p>
-        </section>
-        <section class="get-in-touch line">
-            <p>
-                <span class="get-in-touch__letter10">G</span><span class="get-in-touch__letter9">e</span><span class="get-in-touch__letter8">t</span>&nbsp;<span class="get-in-touch__letter7">i</span><span class="get-in-touch__letter6">n </span>
-                &nbsp;
-                <span class="get-in-touch__letter5">t</span><span class="get-in-touch__letter4">o</span><span class="get-in-touch__letter3">u</span><span class="get-in-touch__letter2">c</span><span class="get-in-touch__letter1">h</span>
-            </p>
-        </section>
-    </main>
+    const handleClickAboutMe = () => {
+        updateAboutMe(!aboutMe);
+    }
 
-    );
+
+    if (aboutMe===false && projects===false && getInTouch===false) {
+
+        return (
+
+        <main class="intro">
+            <section onClick={handleClickAboutMe} class="intro__about-me intro__line">
+                <p>
+                    <span class="intro__about-me--letter1">A</span><span class="intro__about-me--letter2">b</span><span class="intro__about-me--letter3">o</span><span class="intro__about-me--letter4">u</span><span class="intro__about-me--letter5">t </span>
+                    &nbsp;
+                    <span class="intro__about-me--letter6">m</span><span class="intro__about-me--letter7">e</span>
+                </p>
+            </section>
+            <section class="intro__projects intro__line">
+            <p>
+                    <span class="intro__projects--letter4">P</span><span class="intro__projects--letter3">r</span><span class="intro__projects--letter2">o</span><span class="intro__projects--letter1">j</span><span class="intro__projects--letter1">e </span><span class="intro__projects--letter2">c</span><span class="intro__projects--letter3">t</span><span class="intro__projects--letter4">s</span>
+                </p>
+            </section>
+            <section class="intro__get-in-touch intro__line">
+                <p>
+                    <span class="intro__get-in-touch--letter10">G</span><span class="intro__get-in-touch--letter9">e</span><span class="intro__get-in-touch--letter8">t</span>&nbsp;<span class="intro__get-in-touch--letter7">i</span><span class="intro__get-in-touch--letter6">n </span>
+                    &nbsp;
+                    <span class="intro__get-in-touch--letter5">t</span><span class="intro__get-in-touch--letter4">o</span><span class="intro__get-in-touch--letter3">u</span><span class="intro__get-in-touch--letter2">c</span><span class="intro__get-in-touch--letter1">h</span>
+                </p>
+            </section>
+        </main>
+
+        );
+    } else if (aboutMe === true) {
+
+        return (
+            <div>
+               <AboutMe />
+            </div>
+
+        );
+
+    }
 
 }
 
