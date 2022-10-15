8fbdce87e259ab06dd38dd7dd126bc067d9be9b5
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-10-15 10:43:34 +0100

    update font and about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,11 +7,15 @@ const AboutMe = () => {
     if (showDescription == true) {
         return (
             <div className="aboutMe">
-                <h3 className="aboutMe__name">Martinelli Alex</h3>
+                <h3 className="aboutMe__name">const Martinelli_Alex <span className="punctuation">=</span> &#123;</h3>
                 <br></br>
                 <p className="aboutMe__description">
-                    I'm Alex, a junior softaware developer based in Bristol. After many years spent in hospitality I wanted a new career challange that I could enjoy. I love coding: you are always learning something new and solving logical problems. After few months teaching myself on Codecademy I joined the _nology consultancy programme. I hope to find someone that will give me a chance to prove my skills and to improve my coding knowledge.
-                </p>
+                    <span className="key">location</span><span className="punctuation">: "</span><span className="value">Bristol</span><span className="punctuation">",</span>
+                    <br></br>
+                    <span className="key">info</span><span className="punctuation">: "</span><span className="value">
+                    A junior software developer which, after many years spent in hospitality, wants a new career challange that could enjoy. I love coding: you are always learning something new and solving logical problems. After few months teaching myself on Codecademy I joined the _nology consultancy programme. I hope to find an employer that will give me a chance to prove my skills and to improve my coding knowledge.</span><span className="punctuation">"</span></p>
+                <br></br>
+                <h3 className="aboutMe__name">&#125;</h3>   
             </div>
         );
     }
--- src/reusable-elements/card.js
+++ src/reusable-elements/card.js
@@ -1,31 +1,10 @@
 import React, {useState} from "react";
-import GestureFlipView from 'react-native-gesture-flip-card';
 
 const Card = (props) => {
 
-    const renderFront = () => {
-        return (
-          <View style={styles.frontStyle}>
-            <Text style={{fontSize: 25, color: '#fff'}}>{'Front'}</Text>
-          </View>
-        );
-      };
-      
-      const renderBack = () => {
-        return (
-          <View style={styles.backStyle}>
-            <Text style={{fontSize: 25, color: '#fff'}}>{'Back'}</Text>
-          </View>
-        );
-      };
-
     return (
-        <View style={styles.container} className="card">
-        <GestureFlipView width={300} height={500}>
-            {renderBack()}
-            {renderFront()}
-            </GestureFlipView>
-        </View>
+      <div className="card">
+      </div>
       
     )
 
